9259bfe8e546dd99f1f91a2b63cdba8ca84a4576
masterbba <masterbba@gmail.com>
2016-02-17 19:50:39 +0900

    Test

--- TestServer/tcp_server.cpp
+++ TestServer/tcp_server.cpp
@@ -1,65 +1,140 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 #include <WinSock2.h>
 
-#define BUF_SIZE 1024
+#define BUF_SIZE 100
+
+void CompressSockets(SOCKET hSockArr[], int idx, int total);
+void CompressEvents(WSAEVENT hEventkArr[], int idx, int total);
 void ErrorHandling(char* message);
 
 int main(int argc, char* argv[])
 {
-	WSADATA wasData;
-	SOCKET hServSock, hClntSock;	
-	char message[BUF_SIZE];
-	int strLen, i;
+	WSADATA wsaData;
+	SOCKET hServSock, hClntSock;
+	SOCKADDR_IN servAdr, clntAdr;
 
-	SOCKADDR_IN servAddr, clntAddr;
-	int clntAdrSize;
+	SOCKET hSockArr[WSA_MAXIMUM_WAIT_EVENTS];
+	WSAEVENT hEventArr[WSA_MAXIMUM_WAIT_EVENTS];
+	WSAEVENT newEvent;
+	WSANETWORKEVENTS netEvents;
+
+	int numOfClntSock = 0;
+	int strLen, i;
+	int posInfo, startIdx;
+	int clntAdrLen;
+	char msg[BUF_SIZE];
 
 	if (argc != 2)
 	{
-		printf("Usage : %s <port>/n", argv[0]);
+		printf("Usage : %s <port> \n", argv[0]);
 		exit(1);
 	}
-
-	if (WSAStartup(MAKEWORD(2, 2), &wasData) != 0)
-		ErrorHandling("WASStartup() error!");
+	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+		ErrorHandling("WSAStartup() error!");
 
 	hServSock = socket(PF_INET, SOCK_STREAM, 0);
-	if (hServSock == INVALID_SOCKET)
-		ErrorHandling("Socket() error");
+	memset(&servAdr, 0, sizeof(servAdr));
+	servAdr.sin_family = AF_INET;
+	servAdr.sin_addr.s_addr = htonl(INADDR_ANY);
+	servAdr.sin_port = htons(atoi(argv[1]));
 
-	memset(&servAddr, 0, sizeof(servAddr));
-	servAddr.sin_family = AF_INET;
-	servAddr.sin_addr.s_addr = htonl(INADDR_ANY);
-	servAddr.sin_port = htons(atoi(argv[1]));
+	if (bind(hServSock, (SOCKADDR*)&servAdr, sizeof(servAdr)) == SOCKET_ERROR)
+		ErrorHandling("bind() error!");
 
-	if(bind(hServSock, (SOCKADDR*)&servAddr, sizeof(servAddr))==SOCKET_ERROR)
-		ErrorHandling("bind() error");
-
-	if(listen(hServSock, 5)==SOCKET_ERROR)
+	if (listen(hServSock, 5) == SOCKET_ERROR)
 		ErrorHandling("listen() error");
 
-	clntAdrSize = sizeof(clntAddr);
+	newEvent = WSACreateEvent();
+	if (WSAEventSelect(hServSock, newEvent, FD_ACCEPT) == SOCKET_ERROR)
+		ErrorHandling("WSAEventSelect() error");
 
-	for (i = 0; i < 5; i++)
+	hSockArr[numOfClntSock] = hServSock;
+	hEventArr[numOfClntSock] = newEvent;
+	numOfClntSock++;
+
+	while (1)
 	{
-		hClntSock = accept(hServSock, (SOCKADDR*)&clntAddr, &clntAdrSize);
-		if (hClntSock == INVALID_SOCKET)
-			ErrorHandling("accept() error");
-		else
-			printf("Connected client %d \n", i + 1);
+		posInfo = WSAWaitForMultipleEvents(numOfClntSock, hEventArr, FALSE, WSA_INFINITE, FALSE);
+		startIdx = posInfo - WSA_WAIT_EVENT_0;
 
-		while ((strLen = recv(hClntSock, message, BUF_SIZE, 0)) != 0)
-			send(hClntSock, message, strLen, 0);
-		closesocket(hClntSock);
+		for (i = startIdx; i < numOfClntSock; i++)
+		{
+			int sigEventIdx =
+				WSAWaitForMultipleEvents(1, &hEventArr[i], TRUE, 0, FALSE);
+			if ((sigEventIdx == WSA_WAIT_FAILED || sigEventIdx == WSA_WAIT_TIMEOUT))
+			{
+				continue;
+			}
+			else
+			{
+				sigEventIdx = i;
+				WSAEnumNetworkEvents(
+					hSockArr[sigEventIdx], hEventArr[sigEventIdx], &netEvents);
+				if (netEvents.lNetworkEvents & FD_ACCEPT)
+				{
+					if (netEvents.iErrorCode[FD_ACCEPT_BIT] != 0)
+					{
+						puts("Accept Error");
+						break;
+					}
+					clntAdrLen = sizeof(clntAdr);
+					hClntSock = accept(hSockArr[sigEventIdx], (SOCKADDR*)&clntAdr, &clntAdrLen);
+					newEvent = WSACreateEvent();
+					WSAEventSelect(hClntSock, newEvent, FD_READ | FD_CLOSE);
+
+					hEventArr[numOfClntSock] = newEvent;
+					hSockArr[numOfClntSock] = hClntSock;
+					numOfClntSock++;
+					puts("conneted new clinet...");
+				}
+
+				if (netEvents.lNetworkEvents & FD_READ)
+				{
+					if (netEvents.iErrorCode[FD_READ_BIT] != 0)
+					{
+						puts("Read Error");
+						break;
+					}
+					strLen = recv(hSockArr[sigEventIdx], msg, sizeof(msg), 0);
+					send(hSockArr[sigEventIdx], msg, strLen, 0);
+				}
+
+				if (netEvents.lNetworkEvents & FD_CLOSE)
+				{
+					if (netEvents.iErrorCode[FD_CLOSE_BIT] != 0)
+					{
+						puts("Close Error");
+						break;
+					}
+					WSACloseEvent(hEventArr[sigEventIdx]);
+					closesocket(hSockArr[sigEventIdx]);
+
+					numOfClntSock--;
+					CompressSockets(hSockArr, sigEventIdx, numOfClntSock);
+					CompressEvents(hEventArr, sigEventIdx, numOfClntSock);
+				}
+			}
+		}
 	}
 
-	closesocket(hServSock);
 	WSACleanup();
 	return 0;
 }
 
+void CompressSockets(SOCKET hSockArr[], int idx, int total)
+{
+	int i;
+	for (i = idx; i < total; i++)
+		hSockArr[i] = hSockArr[i + 1];
+}
+
+void CompressEvents(WSAEVENT hEventkArr[], int idx, int total)
+{
+	int i;
+	for (i = idx; i < total; i++)
+		hEventkArr[i] = hEventkArr[i + 1];
+}
 void ErrorHandling(char* message)
 {
 	fputs(message, stderr);
